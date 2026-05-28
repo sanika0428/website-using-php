9a3499f7bfba4d35fc9944f26e872786f3b584d5
Sanika Digambar Gaonkar <sanikagaonkar0428@gmail.com>
2026-05-28 12:26:26 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+include 'db.php';
+
+if(isset($_POST['login'])){
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users
+            WHERE email='$email'
+            AND password='$password'";
+
+    $result = mysqli_query($conn,$sql);
+
+    if(mysqli_num_rows($result) > 0){
+        echo "Login Successful";
+    } else {
+        echo "Invalid Email or Password";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>Login Form</h2>
+
+<form method="POST">
+
+    <input type="email" name="email" placeholder="Email" required><br><br>
+
+    <input type="password" name="password" placeholder="Password" required><br><br>
+
+    <button type="submit" name="login">Login</button>
+
+</form>
+
+</body>
+</html>
